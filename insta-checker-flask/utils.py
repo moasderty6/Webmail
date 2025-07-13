a52dc87b7d2796a83dc4a5d6c4af5e61a35ae821
moasderty6 <moooody.18.18.18@gmail.com>
2025-07-13 12:16:59 +0300

    Update utils.py

--- insta-checker-flask/utils.py
+++ insta-checker-flask/utils.py
@@ -1,41 +1,24 @@
 import subprocess
 import itertools
-import string
 
 def generate_emails(masked_email):
     prefix, domain = masked_email.split("@")
 
-    # استخراج مواقع النجوم
+    # حدد مواقع النجوم
     star_indices = [i for i, c in enumerate(prefix) if c == '*']
     num_stars = len(star_indices)
 
     if num_stars == 0:
-        return [masked_email]  # لا حاجة للتخمين
+        yield masked_email
+        return
 
-    chars = string.ascii_lowercase + string.digits
+    # توليد كل الاحتمالات: أحرف + أرقام + _ .
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789_.'
     combinations = itertools.product(chars, repeat=num_stars)
 
-    emails = []
     for combo in combinations:
         guess = list(prefix)
         for idx, char in zip(star_indices, combo):
             guess[idx] = char
         email = ''.join(guess) + '@' + domain
-        emails.append(email)
-        if len(emails) >= 30:
-            break
-    return emails
-
-async def check_email_with_holehe(emails, username):
-    results = []
-    for email in emails:
-        try:
-            output = subprocess.check_output(
-                ["python3", "holehe_wrapper/run.py", email],
-                text=True
-            )
-            if "Instagram" in output:
-                results.append(email)
-        except Exception:
-            continue
-    return results
+        yield email
