4f4ff32fe4183d2e0da98758c351581a5a8763b9
moasderty6 <moooody.18.18.18@gmail.com>
2025-07-13 12:18:03 +0300

    Update run.py

--- insta-checker-flask/holehe_wrapper/run.py
+++ insta-checker-flask/holehe_wrapper/run.py
@@ -1,7 +1,9 @@
 import sys
+
 email = sys.argv[1]
-# سكربت وهمي فقط للعرض
-if "gmail" in email:
+
+# سكربت وهمي: بدّله لاحقًا بـ holehe الحقيقي
+if "insta" in email or "gram" in email:
     print("Found on Instagram")
 else:
     print("Not found")
